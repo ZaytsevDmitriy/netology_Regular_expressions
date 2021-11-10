f7d21192b081be0c291d8a3f72244a9eb5343389
Dmitriy Zaytsev <dmitrijzaytsev85@gmail.com>
2021-11-10 21:18:22 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from pprint import pprint
+# читаем адресную книгу в формате CSV в список contacts_list
+import csv
+with open("phonebook_raw.csv", encoding="utf-8" ) as f:
+    rows = csv.reader(f, delimiter=",")
+    contacts_list = list(rows)
+pprint(contacts_list)
+
+
